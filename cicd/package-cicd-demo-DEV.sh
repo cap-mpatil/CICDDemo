c73bc0cf7957ef586b124c8920f672f04dedea51
Murugesh Patil (ns) <mpatil@cap.org>
2019-03-31 15:41:44 -0500

    bucket prefix

--- cicd/package-cicd-demo-DEV.sh
+++ cicd/package-cicd-demo-DEV.sh
@@ -10,7 +10,7 @@ securityGroupId=sg-0218decf8ff3c408a
 apiDeployStageName=dev
 
 s3BucketForArtifacts=capdev-deployment
-s3FolderPrefixForArtifacts=cicd-demo-pipeline
+s3FolderPrefixForArtifacts=cicd-demo-pipeline/lambda-jars
 
 RELEASE_VERSION_NUMBER=1.0
 
